fadc83153e353103b7728b5599f9d54f32adeb6e
Chris Le Sueur <c.m.lesueur@gmail.com>
2023-12-07 00:50:18 +0000

    Use more ints on Day 6

--- src/bin/day-06.rs
+++ src/bin/day-06.rs
@@ -3,11 +3,12 @@ use itertools::Itertools;
 use aoc2023::common::{read_input_lines, strs_to_nums};
 
 #[inline]
-fn n_winning_times(time: f64, best_distance: f64) -> i64 {
-    let discriminant = (time * time - 4.0 * best_distance).sqrt();
-    let left = (0.5 * (time - discriminant)).floor() + 1.0;
-    let right = (0.5 * (time + discriminant)).ceil() - 1.0;
-    (right - left + 1.0) as i64
+fn n_winning_times(time: u64, best_distance: u64) -> i64 {
+    let discriminant = ((time * time - 4 * best_distance) as f64).sqrt();
+    // .floor + 1 because
+    let left = (0.5 * (time as f64 - discriminant)).floor() as i64 + 1;
+    let right = (0.5 * (time as f64 + discriminant)).ceil() as i64 - 1;
+    right - left + 1
 }
 
 fn main() {
@@ -31,14 +32,12 @@ fn main() {
         .split_ascii_whitespace()
         .skip(1)
         .join("")
-        .parse::<f64>()
+        .parse::<u64>()
         .unwrap();
     let d2 = lines[1]
         .split_ascii_whitespace()
         .skip(1)
         .join("")
-        .parse::<f64>()
+        .parse::<u64>()
         .unwrap();
-
-    println!("{}", n_winning_times(t2, d2));
 }
