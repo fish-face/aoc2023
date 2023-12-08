f7c061ac694e106981214cc9ca53962f83c56542
Chris Le Sueur <c.m.lesueur@gmail.com>
2023-12-08 00:16:30 +0000

    Don't use Itertools::counts() to get counts on Day 7

--- src/bin/day-07.rs
+++ src/bin/day-07.rs
@@ -1,5 +1,4 @@
-use std::cmp::Ordering;
-use itertools::Itertools;
+use std::cmp::{max, Ordering};
 use aoc2023::common::read_input_lines;
 
 #[derive(Debug, PartialOrd, PartialEq, Eq, Ord, Clone, Copy)]
@@ -31,8 +30,9 @@ impl Play {
     #[inline]
     fn from_str(line: String) -> Play {
         let (cards, bid) = line.split_once(' ').unwrap();
-        let hand = Play::hand(cards);
-        let hand2 = Play::hand2(cards);
+        let (counts, joker_count) = Self::counts(cards);
+        let hand = Play::hand(counts);
+        let hand2 = Play::hand2(counts, joker_count);
         let cards2 = cards.bytes().map(Play::byte_to_card2).collect();
         let cards = cards.bytes().map(Play::byte_to_card).collect();
         let bid = bid.parse().unwrap();
@@ -82,42 +82,65 @@ impl Play {
     }
 
     #[inline]
-    fn hand(cards: &str) -> Hand {
-        let counts = cards
-            .bytes()
-            .counts();
-        let counts = counts
-            .iter()
-            .map(|(_char, count)| count)
-            .sorted()
-            .rev()
-            .collect::<Vec<_>>();
-        match *counts[0] {
+    fn counts(cards: &str) -> ((u8, u8), u8) {
+        let mut counts = [0; 5];
+
+        let bytes = &cards.as_bytes()[0..5];
+        let mut map = [255_u8; 256];
+        for i in 0..5 {
+            let c = bytes[i] as usize;
+            if map[c] == 255 {
+                map[c] = i as u8;
+                counts[i] += 1;
+            } else {
+                counts[map[c] as usize] += 1;
+            }
+        }
+        let mut largest = 0;
+        let mut twos = 0;
+        for i in 0..5 {
+            largest = max(largest, counts[i]);
+            if counts[i] == 2 {
+                twos += 1;
+            }
+        }
+        let jokers = match map['J' as usize] as usize {
+            255 => 0,
+            i => counts[i],
+        };
+        if largest > 2 {
+            if twos > 0 {
+                ((largest, 2), jokers)
+            } else {
+                ((largest, 1), jokers)
+            }
+        } else if twos == 2 {
+            ((2, 2), jokers)
+        } else if twos == 1 {
+            ((2, 1), jokers)
+        } else {
+            ((1, 1), jokers)
+        }
+    }
+
+    #[inline]
+    fn hand(counts: (u8, u8)) -> Hand {
+        match counts.0 {
             5 => Hand::Five,
             4 => Hand::Four,
-            3 => if *counts[1] == 2 { Hand::FullHouse } else { Hand::Three },
-            2 => if *counts[1] == 2 { Hand::TwoPair } else { Hand::OnePair },
+            3 => if counts.1 == 2 { Hand::FullHouse } else { Hand::Three },
+            2 => if counts.1 == 2 { Hand::TwoPair } else { Hand::OnePair },
             1 => Hand::Nowt,
             _ => panic!(),
         }
     }
 
     #[inline]
-    fn hand2(cards: &str) -> Hand {
-        let counts = cards
-            .bytes()
-            .counts();
-        let joker_count = *counts.get(&('J' as u8)).unwrap_or(&0);
-        let counts = counts
-            .iter()
-            .map(|(_char, count)| count)
-            .sorted()
-            .rev()
-            .collect::<Vec<_>>();
+    fn hand2(counts: (u8, u8), joker_count: u8) -> Hand {
         return match joker_count {
             5 | 4 => Hand::Five,
-            3 => match *counts[1] {
-                // counts[0] is the three jokers
+            3 => match counts.1 {
+                // counts.0 is the three jokers
 
                 // JJJXX --> XXXXX
                 2 => Hand::Five,
@@ -125,9 +148,9 @@ impl Play {
                 1 => Hand::Four,
                 _ => panic!(),
             },
-            2 => match *counts[0] {
+            2 => match counts.0 {
                 3 => Hand::Five,
-                2 => match *counts[1] {
+                2 => match counts.1 {
                     // XXJJY
                     2 => Hand::Four,
                     // XYZJJ
@@ -135,12 +158,12 @@ impl Play {
                 },
                 _ => { panic!(); }
             },
-            1 => match *counts[0] {
+            1 => match counts.0 {
                 // JXXXX --> XXXXX
                 4 => Hand::Five,
                 // JXXXY --> XXXXY
                 3 => Hand::Four,
-                2 => match *counts[1] {
+                2 => match counts.1 {
                     // JXXYY --> XXXYY
                     2 => Hand::FullHouse,
                     // JXXYZ --> XXXYZ
@@ -150,11 +173,11 @@ impl Play {
                 1 => Hand::OnePair,
                 _ => panic!(),
             },
-            0 => match *counts[0] {
+            0 => match counts.0 {
                 5 => Hand::Five,
                 4 => Hand::Four,
-                3 => if *counts[1] == 2 { Hand::FullHouse } else { Hand::Three },
-                2 => if *counts[1] == 2 { Hand::TwoPair } else { Hand::OnePair },
+                3 => if counts.1 == 2 { Hand::FullHouse } else { Hand::Three },
+                2 => if counts.1 == 2 { Hand::TwoPair } else { Hand::OnePair },
                 1 => Hand::Nowt,
                 _ => panic!(),
             },
@@ -173,24 +196,26 @@ impl PartialOrd for Play {
 }
 
 fn main () {
-    let lines = read_input_lines().expect("Could not read input file");
+    for _ in 0..1{
+        let lines = read_input_lines().expect("Could not read input file");
 
-    let plays = lines.map(Play::from_str).collect::<Vec<_>>();
-    let part1: usize = plays
-        .iter()
-        .sorted()
-        .enumerate()
-        .map(|(i, play)| (i+1) * play.bid)
-        .sum();
-    println!("{}", part1);
-    let part2: usize = plays
-        .iter()
-        .sorted_by(|play1, play2| match play1.hand2.cmp(&play2.hand2) {
+        let mut plays = lines.map(Play::from_str).collect::<Vec<_>>();
+        plays.sort();
+        let part1: usize = plays
+            .iter()
+            .enumerate()
+            .map(|(i, play)| (i+1) * play.bid)
+            .sum();
+        println!("{}", part1);
+        plays.sort_by(|play1, play2| match play1.hand2.cmp(&play2.hand2) {
             Ordering::Equal => play1.cards2.cmp(&play2.cards2),
             ord => ord,
-        })
-        .enumerate()
-        .map(|(i, play)| (i+1) * play.bid)
-        .sum();
-    println!("{:?}", part2);
+        });
+        let part2: usize = plays
+            .iter()
+            .enumerate()
+            .map(|(i, play)| (i+1) * play.bid)
+            .sum();
+        println!("{:?}", part2);
+    }
 }
