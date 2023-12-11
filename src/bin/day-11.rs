733cd83c61409d0576c7883db5d595dbffb7c22e
Chris Le Sueur <c.m.lesueur@gmail.com>
2023-12-11 23:58:01 +0000

    Day 11: slight speedup by skipping most iterations

--- src/bin/day-11.rs
+++ src/bin/day-11.rs
@@ -1,9 +1,6 @@
 use aoc2023::common::read_input_lines;
 
 fn main() {
-    let mut width = 0;
-    let mut height = 0;
-
     let mut galaxies = 0;
     let mut galaxies_x = vec![0; 140];
     let mut galaxies_y = vec![0; 140];
@@ -14,51 +11,38 @@ fn main() {
                 galaxies_x[x] += 1;
                 galaxies_y[y] += 1;
             }
-            if height == 0 {
-                width += 1;
-            }
         }
-        height += 1;
     }
 
-    let mut idx = 0;
-    let mut dist = 0;
-    let mut skips = 0;
     let mut part1 = 0;
     let mut part2 = 0_usize;
 
-    for x in 0..width {
-        let n = galaxies_x[x];
-        if n == 0 {
-            skips += 1;
-        } else {
-            let multiple = (galaxies - idx) * idx;
-            part1 += (dist + skips) * multiple;
-            part2 += (dist + skips * (1_000_000 - 1)) * multiple;
-            dist = 0;
-            skips = 0;
-            idx += n;
-        }
-        dist += 1;
+    let mut idx = galaxies_x[0];
+    let mut prev_x = 0;
+    for (x, n) in galaxies_x[1..].iter().enumerate().filter(|(_, n)| **n > 0) {
+        let x = x + 1;
+        let skips = x - prev_x - 1;
+
+        let multiple = (galaxies - idx) * idx;
+        part1 += (1 + skips * 2) * multiple;
+        part2 += (1 + skips * (1_000_000)) * multiple;
+
+        idx += n;
+        prev_x = x;
     }
 
-    idx = 0;
-    dist = 0;
-    skips = 0;
+    let mut idx = galaxies_y[0];
+    let mut prev_y = 0;
+    for (y, n) in galaxies_y[1..].iter().enumerate().filter(|(_, n)| **n > 0) {
+        let y = y + 1;
+        let skips = y - prev_y - 1;
 
-    for y in 0..height {
-        let n = galaxies_y[y];
-        if n == 0 {
-            skips += 1;
-        } else {
-            let multiple = (galaxies - idx) * idx;
-            part1 += (dist + skips) * multiple;
-            part2 += (dist + skips * (1_000_000 - 1)) * multiple;
-            dist = 0;
-            skips = 0;
-            idx += n;
-        }
-        dist += 1;
+        let multiple = (galaxies - idx) * idx;
+        part1 += (1 + skips * 2) * multiple;
+        part2 += (1 + skips * (1_000_000)) * multiple;
+
+        idx += n;
+        prev_y = y;
     }
 
     println!("{}\n{}", part1, part2);
