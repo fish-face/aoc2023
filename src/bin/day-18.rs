6cd7e0d7870a839479742844595d1adaa53bf410
Chris Le Sueur <c.m.lesueur@gmail.com>
2023-12-19 14:02:18 +0000

    Day 18: reject fun rectangular slicing, embrace boring shoelace formula

--- src/bin/day-18.rs
+++ src/bin/day-18.rs
@@ -1,9 +1,5 @@
-use std::cmp::{max, min};
-use std::collections::{BTreeSet};
-use std::iter::{FilterMap, Map};
 use std::mem::transmute;
-use itertools::{Group, Groups, Itertools};
-use aoc2023::common::{read_input_bytes, read_input_lines};
+use aoc2023::common::read_input_lines;
 use aoc2023::coord::Pt;
 
 #[derive(PartialOrd, PartialEq, Eq, Ord, Copy, Clone)]
@@ -16,114 +12,65 @@ struct Instruction {
 }
 
 impl Instruction {
-    fn from_bytes(mut bytes: impl Iterator<Item=u8>) -> (Self, Self) {
-        let dir1 = match bytes.next().unwrap() {
+    fn from_str(s: String) -> (Self, Self) {
+        let s = s.as_bytes();
+        let dir1 = match s[0] {
             b'U' => Dir::N,
             b'R' => Dir::E,
             b'D' => Dir::S,
             b'L' => Dir::W,
             _ => {panic!("nope");}
         };
-        bytes.nth(0);
-        let dist_bytes = bytes.by_ref().take_while(|b| *b != b' ');
         let mut dist1 = 0;
-        for b in dist_bytes {
+        let mut part2 = 0;
+        for (i, b) in s[2..].iter().enumerate() {
+            if *b == b' ' {
+                part2 = i;
+                break;
+            }
             dist1 *= 10;
-            dist1 += (b - b'0') as isize;
+            dist1 += (*b - b'0') as isize;
         }
-        bytes.nth(1);
-        // todo this heap allocates making all the bytes and unsafeness pointless
-        let dist2_bytes = bytes.by_ref().take(5).collect();
-        let dist2 = unsafe { isize::from_str_radix(&String::from_utf8_unchecked(dist2_bytes), 16).unwrap() };
-        let dir2 = unsafe { transmute(bytes.next().unwrap() - b'0') };
+        let mut dist2 = 0_isize;
+        for b in s[part2+5..part2+10].iter() {
+            dist2 *= 16;
+            dist2 += (*b as char).to_digit(16).expect(format!("invalid: {}", b).as_str()) as isize;
+        }
+        let dir2 = unsafe { transmute(s[part2+10] - b'0') };
 
         (Instruction{ dir: dir1, dist: dist1}, Instruction{dist: dist2, dir: dir2})
     }
 }
 
-fn area(instructions: impl Iterator<Item=Instruction>) -> usize {
-    let mut current = Pt(0_isize, 0_isize);
-    let mut x_crossings = BTreeSet::new();
-    let mut y_crossings = BTreeSet::new();
-
-    let mut perimeter = 0;
-
-    for instruction in instructions {
-        let next = match instruction.dir {
-            Dir::N => {
-                let next = Pt(current.0, current.1 - instruction.dist);
-                x_crossings.insert(current.0);
-                next
-            }
-            Dir::S => {
-                let next = Pt(current.0, current.1 + instruction.dist);
-                x_crossings.insert(current.0);
-                next
-            }
-            Dir::E => {
-                let next = Pt(current.0 + instruction.dist, current.1);
-                y_crossings.insert((current.1, (current.0, next.0)));
-                next
-            }
-            Dir::W => {
-                let next = Pt(current.0 - instruction.dist, current.1);
-                y_crossings.insert((current.1, (next.0, current.0)));
-                next
-            }
-        };
-
-        current = next;
-        perimeter += instruction.dist;
-    }
-
-    let mut xc_prev = x_crossings.pop_first().unwrap();
-    let mut yc_prev = isize::MAX;
-
-    let mut acc = 0;
-
-    for xc in x_crossings.iter() {
-        let width = xc - xc_prev;
-        let mut counting = false;
-        let mut height = 0;
-        for (yc, (xra, xrb)) in y_crossings.iter() {
-
-            if !(xc > xra && xc <= xrb) {
-                continue;
-            }
-
-            if *yc >= yc_prev {
-                height = *yc - yc_prev;
-            }
-
-            // println!("{xc},{yc} {width}x{height} left {}{xra}--{xrb} --> {counting}", if going_right { '+' } else { '-' });
-            if counting {
-                acc += width * (height + 0);
-            }
-            counting = !counting;
-
-            yc_prev = *yc;
-        }
-
-        xc_prev = *xc;
-    }
-
-    (acc + perimeter / 2 + 1).try_into().unwrap()
+#[inline]
+fn increment_area(pt: &mut Pt<isize>, area: &mut isize, perimeter: &mut isize, inst: Instruction) {
+    let next = match inst.dir {
+        Dir::N => Pt(pt.0, pt.1 - inst.dist),
+        Dir::S => Pt(pt.0, pt.1 + inst.dist),
+        Dir::E => Pt(pt.0 + inst.dist, pt.1),
+        Dir::W => Pt(pt.0 - inst.dist, pt.1),
+    };
+    *area += pt.0 * next.1 - pt.1 * next.0;
+    *pt = next;
+    *perimeter += inst.dist;
 }
 
 fn main() {
-    let input = read_input_bytes()
-        .group_by(|b| *b == b'\n');
-    let input = input
-        .into_iter()
-        .filter_map(|(newline, line)| if newline {
-            None
-        } else {
-            Some(line)
-        });
+    let input = read_input_lines().unwrap();
     let instructions = input
-        .map(Instruction::from_bytes).collect::<Vec<_>>();
+        .map(Instruction::from_str);
 
-    let part1 = area(instructions.iter().map(|(a, b)| *a));
-    let part2 = area(instructions.into_iter().map(|(a, b)| b));
-    println!("{part1}\n{part2}");
+    let mut p1_pt = Pt(0, 0);
+    let mut p1_area = 0;
+    let mut p1_perimeter = 0;
+    let mut p2_pt = Pt(0, 0);
+    let mut p2_area = 0;
+    let mut p2_perimeter = 0;
+    for (p1_inst, p2_inst) in instructions {
+        increment_area(&mut p1_pt, &mut p1_area, &mut p1_perimeter, p1_inst);
+        increment_area(&mut p2_pt, &mut p2_area, &mut p2_perimeter, p2_inst);
+    }
+    println!("{}", (p1_area + p1_perimeter) / 2 + 1);
+    println!("{}", (p2_area + p2_perimeter) / 2 + 1);
 }
+
