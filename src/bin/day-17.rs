da2e6fd1fa645293ad95989ba0234a34f5b8ccf1
Chris Le Sueur <c.m.lesueur@gmail.com>
2023-12-19 23:49:45 +0000

    Day 17: optimise by adding more points to the queue per iteration

--- src/bin/day-17.rs
+++ src/bin/day-17.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+use std::cmp::{min, Ordering};
 use std::collections::BinaryHeap;
 use itertools::Itertools;
 use aoc2023::common::read_input_bytes;
@@ -8,27 +8,35 @@ use aoc2023::grid::Grid;
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 enum Dir { N, E, S, W }
 
+#[inline]
+fn flip(d: Dir) -> Dir {
+    match d {
+        Dir::N => Dir::S,
+        Dir::E => Dir::W,
+        Dir::S => Dir::N,
+        Dir::W => Dir::E,
+    }
+}
+
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 struct State {
     pos: Pt<usize>,
-    gone_straight: usize,
     straight_dir: Dir,
 }
 
 impl State {
-    fn idx(&self, width: usize, height: usize, max_dist: usize) -> usize {
-        self.pos.0 + self.pos.1 * width + self.gone_straight * width * height + (self.straight_dir as usize) * width * height * max_dist
+    fn idx(&self, width: usize, height: usize) -> usize {
+        self.pos.0 + self.pos.1 * width + (self.straight_dir as usize) * width * height
     }
 }
 
-#[derive(Clone, Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq, Debug)]
 struct CostedState(State, u16);
 
 impl Ord for CostedState {
     fn cmp(&self, other: &Self) -> Ordering {
         // flipped because Heap is a max-heap
         other.1.cmp(&self.1)
-            .then_with(|| self.0.gone_straight.cmp(&other.0.gone_straight))
             .then_with(|| self.0.pos.0.cmp(&other.0.pos.0))
             .then_with(|| self.0.pos.1.cmp(&other.0.pos.1))
     }
@@ -41,53 +49,70 @@ impl PartialOrd for CostedState {
 }
 
 fn search(map: &Grid<u16>, start: Pt<usize>, end: Pt<usize>, min_straight: usize, max_straight: usize) -> u16 {
-    let start_state = State { pos: start, gone_straight: 0, straight_dir: Dir::E };
     let mut queue = BinaryHeap::<CostedState>::new();
-    queue.push(CostedState(start_state, 0));
     let mut best = vec![None; map.width + map.width * map.height + map.width * map.height * max_straight + map.width * map.height * max_straight * 4];
-    best[start_state.idx(map.width, map.height, max_straight)] = Some(0);
+
+    let start_state = State { pos: start, straight_dir: Dir::E };
+    queue.push(CostedState(start_state, 0));
+    best[start_state.idx(map.width, map.height)] = Some(0);
+
+    let start_state = State { pos: start, straight_dir: Dir::S };
+    queue.push(CostedState(start_state, 0));
+    best[start_state.idx(map.width, map.height)] = Some(0);
+
+    // let mut prev_map = std::collections::HashMap::<State, State>::new();
 
     while let Some(CostedState(state, cost)) = queue.pop() {
         if state.pos == end {
+            // let mut debugmap = Grid::new(map.width, map.height);
+            // let mut debugstate = state;
+            // loop {
+            //     let opt = prev_map.get(&debugstate);
+            //     if opt.is_none() {break;}
+            //     debugstate = *opt.unwrap();
+            //     debugmap[debugstate.pos] = true;
+            // }
+            // println!("{}\n", debugmap.map(|x| if *x {'#'} else {'.'}).to_string(Some("")));
             return cost;
         }
 
         for dir in [Dir::N, Dir::E, Dir::S, Dir::W] {
-            if dir != state.straight_dir && state.gone_straight < min_straight {
+            if dir == state.straight_dir || dir == flip(state.straight_dir) {
                 continue;
             }
-            let straight_dist = if dir == state.straight_dir {
-                state.gone_straight + 1
-            } else {
-                1
+
+            let max_movement = match dir {
+                Dir::N => state.pos.1,
+                Dir::E => map.width - state.pos.0 - 1,
+                Dir::S => map.height - state.pos.1 - 1,
+                Dir::W => state.pos.0,
             };
-            if straight_dist > max_straight { continue; }
+            if max_movement < min_straight {
+                continue;
+            }
 
-            let next_pos = match dir {
-                Dir::N => {
-                    if state.straight_dir == Dir::S || state.pos.1 == 0 { continue; }
-                    Pt(state.pos.0, state.pos.1 - 1)
-                }
-                Dir::E => {
-                    if state.straight_dir == Dir::W || state.pos.0 == map.width - 1 { continue; }
-                    Pt(state.pos.0 + 1, state.pos.1)
-                }
-                Dir::S => {
-                    if state.straight_dir == Dir::N || state.pos.1 == map.height - 1 { continue; }
-                    Pt(state.pos.0, state.pos.1 + 1)
-                }
-                Dir::W => {
-                    if state.straight_dir == Dir::E || state.pos.0 == 0 { continue; }
-                    Pt(state.pos.0 - 1, state.pos.1)
-                }
-            };
+            let mut next_cost = cost;
+            for dist in 1..=min(max_movement, max_straight) {
+                let next_pos = match dir {
+                    Dir::N => Pt(state.pos.0, state.pos.1 - dist),
+                    Dir::E => Pt(state.pos.0 + dist, state.pos.1),
+                    Dir::S => Pt(state.pos.0, state.pos.1 + dist),
+                    Dir::W => Pt(state.pos.0 - dist, state.pos.1),
+                };
+                assert!(map.contains(next_pos));
 
-            let next_state = State { pos: next_pos, gone_straight: straight_dist, straight_dir: dir };
-            let next_cost = cost + map[next_pos];
+                next_cost += map[next_pos];
 
-            if next_cost < best[next_state.idx(map.width, map.height, max_straight)].unwrap_or(u16::MAX) {
-                queue.push(CostedState(next_state, next_cost));
-                best[next_state.idx(map.width, map.height, max_straight)] = Some(next_cost);
+                if dist < min_straight { continue; }
+
+                let next_state = State { pos: next_pos, straight_dir: dir };
+
+                if next_cost < best[next_state.idx(map.width, map.height)].unwrap_or(u16::MAX) {
+                    // println!("{:?}", CostedState(next_state, next_cost));
+                    queue.push(CostedState(next_state, next_cost));
+                    best[next_state.idx(map.width, map.height)] = Some(next_cost);
+                    // prev_map.insert(next_state, state);
+                }
             }
         }
     }
